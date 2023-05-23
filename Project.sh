243304948d1ba5f1cc6e23ed12d0f39bdded3920
L-bytes <62182894+L-bytes@users.noreply.github.com>
2023-05-23 11:32:32 +0200

    Add files via upload

---
+++ Project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Set job requirements
+#SBATCH -N 1
+#SBATCH -t 6:00:00
+ 
+#Loading modules
+module load 2022
+module load R
+ 
+#Create output directory on scratch
+mkdir "$TMPDIR"/"$SLURM_JOBID"
+
+#Copy input file to scratch
+cp -r "$PWD"/data "$TMPDIR"/"$SLURM_JOBID"
+cp -r "$PWD"/src "$TMPDIR"/"$SLURM_JOBID"
+
+export R_LIBS=$HOME/rpackages:$R_LIBS
+ 
+#Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
+Rscript --no-save --slave "$PWD"/src/Main.R "$TMPDIR/$SLURM_JOBID"
+ 
+#Copy output directory from scratch to home
+mkdir "$PWD"/"$SLURM_JOBID"
+mkdir "$PWD/$SLURM_JOBID"/output
+cp -r "$TMPDIR"/"$SLURM_JOBID"/output "$PWD/$SLURM_JOBID/output"
+cp -r "$TMPDIR"/"$SLURM_JOBID"/figures "$PWD/$SLURM_JOBID/output"
+cp -r "$TMPDIR"/"$SLURM_JOBID"/rdata "$PWD/$SLURM_JOBID/output"
