ed55168d1c5ea5bcc69ead23ed29ce3a77b78795
L-bytes <marlenevlaar@outlook.com>
2023-11-03 00:47:32 +0100

    Final updates

--- src/coexpression_analysis.R
+++ src/coexpression_analysis.R
@@ -1,51 +1,29 @@
-### Coexpression analysis workflow using the WGCNA package. The WGCNA package has a tutorial itself, which also includes some
-### preparatory steps to check for sample outliers etc. As these are very specific to the input data, we have not included them
-### as part of the workflow here but instead assume these have been followed already. We recommend going back to the original
-### documentation for the quality check (and also the general workflow, as we generally followed their guidelines):
-### (https://horvath.genetics.ucla.edu/html/CoexpressionNetwork/Rpackages/WGCNA/Tutorials/)
-### The quality check for our dataset can be found in "coexpression_analysis_prep.R"
-
 #' Coexpression analysis workflow using the WGCNA package
 #' 
-#' @param d Input a dataframe with expression values, using the Protein IDs as rows, samples as columns.
-#' @param d.log2fc log2FC values of the groups
-#' @param outfolder Output folder for figures
+#' @param d Input a dataframe with expression values, using the gene symbols as rows, samples as columns.
+#' @param d.log2fc log2FC values
+#' @param outfolder Output folder for tables
+#' @param figfolder Output folder for figures
+#' @param i Number of run
+#' @param stage Exploration or validation round
 #' @return Coexpression network of \code{d} and significance of log2FC value distribution(s) within each of the modules
 
 coexpression.analysis <- function(d, d.log2fc, outfolder, figfolder, i, stage, power=FALSE){
-  # Pick acceptable value for beta
-  # Choose a set of soft-thresholding powers (beta)
   powers <- c(c(1:11), seq(from=12, to=26, by=2))
-  # Call the network topology analysis function
   soft.tresh <- pickSoftThreshold(d, powerVector=powers, verbose=5)
-  # Plot scale-free topology parameters as function of beta
   png(filename=paste0(figfolder, '/Scale_free_threshold.png'), width=960, height=480)
   par(mfrow = c(1,2))
   cex1 = 0.9
-  # Scale-free topology fit index as a function of beta
   plot(soft.tresh $fitIndices[,1], -sign(soft.tresh $fitIndices[,3])*soft.tresh $fitIndices[,2], xlab="Beta",ylab="Scale Free Topology Model Fit, R^2",type="n", main = paste("Scale independence"))
   text(soft.tresh $fitIndices[,1], -sign(soft.tresh $fitIndices[,3])*soft.tresh $fitIndices[,2], labels=powers,cex=cex1,col="red")
-  # this line corresponds to using an R^2 cut-off of h
   abline(h=0.80,col="red")
-  # Mean connectivity as a function of beta
   plot(soft.tresh $fitIndices[,1], soft.tresh $fitIndices[,5], xlab="Beta", ylab="Mean Connectivity", type="n", main = paste("Mean connectivity"))
   text(soft.tresh $fitIndices[,1], soft.tresh $fitIndices[,5], labels=powers, cex=cex1,col="red")
   abline(h=150,col="red")
   dev.off()
-  # If power not provided, pick from the scale independence analysis
   if (!power){
-    # Pick first value where scale independence is higher than 0.8
     sf.values <- -sign(soft.tresh $fitIndices[,3])*soft.tresh $fitIndices[,2]
     sf.values2 <- -sign(soft.tresh $fitIndices[,5])
-    #power <- 0
-    
-    #    for (i in 1:length(sf.values)){
-    #      print(sf.values[i])
-    #      if (sf.values[i] > 0.9 && sf.values[i] > sf.values[i-1] && sf.values[i] > sf.values[i+1]){
-    #        power <- i
-    #        break
-    #      }
-    #    }
     threshold <- 0.8
     print(paste0("Threshold: ", threshold))
     sf.bool <- sf.values > threshold
@@ -53,57 +31,43 @@ coexpression.analysis <- function(d, d.log2fc, outfolder, figfolder, i, stage, p
     power <- soft.tresh $fitIndices[,1][sf.bool][1]
   }
   print(paste0("Power : ", power))
-  # Create network from chosen value of beta
   rownames(d) <- paste(rownames(d), length(groups), sep='_')
-  #  allowWGCNAThreads(nThreads = NULL)
-  #  enableWGCNAThreads(nThreads = NULL)
-  #  print(WGCNAnThreads())
   dSplit <- 2
   mergeCut <- 0.25
   print(paste0("deepSplit: ", dSplit))
   print(paste0("mergeCutHeight: ", mergeCut))
   coex.net <- blockwiseModules(d, power=power,
-                               TOMType="unsigned", #minModuleSize=30,
+                               TOMType="unsigned", minModuleSize=30,
                                reassignThreshold=0, mergeCutHeight=mergeCut,
-                               numericLabels=TRUE, pamRespectsDendro=FALSE, deepSplit = dSplit, nPreclusteringCenters = 5,
+                               numericLabels=TRUE, pamRespectsDendro=FALSE, deepSplit = dSplit,
                                saveTOMs=TRUE, saveTOMFileBase=paste0("output/", i, "/", stage, "/rdata/coexpression_discovery"), verbose=3, maxBlockSize=ncol(d))
-  # Merge M18 (lightgreen) into M9 (magenta), since they were highly similar
-  # coex.net$colors <- replace(coex.net$colors, coex.net$colors==18, 9)
-  # Plot modules and gene hierarchical clustering
   moduleColors <- labels2colors(coex.net$colors)
   modules <- levels(as.factor(moduleColors))
   n.modules <- length(modules)
   par(mar = c(1, 1, 1, 1))
   png(file=paste0(figfolder, '/module_dendrogram.png'), width=1920, height=1080)
   plotDendroAndColors(coex.net$dendrograms[[1]], moduleColors,
-                      "Module colors", dendroLabels=FALSE, hang=0.03, addGuide=TRUE, guideHang=0.05, cex.lab = 2, cex.axis = 2, cex.main = 2, cex.colorLabels = 2)#,
-  #abHeight=0.85, abCol='red')
+                      "Module colors", dendroLabels=FALSE, hang=0.03, addGuide=TRUE, guideHang=0.05, cex.lab = 2, cex.axis = 2, cex.main = 2, cex.colorLabels = 2)
   dev.off()
-  # Plot distribution of eigengene expression values for each module, and calculate significance of differential expression within the modules
   MEs <- coex.net$MEs
   ME.WT <- MEs[str_detect(rownames(MEs), 'WT'),]
   ME.SNV <- MEs[str_detect(rownames(MEs), 'SNV'),]
   
-  
-  # Table with P-values for distribution of log2 values
   pvals.modules <- data.frame(matrix(nrow=n.modules, ncol=ncol(d.log2fc)))
   colnames(pvals.modules) <- colnames(d.log2fc)
   rownames(pvals.modules) <- modules
   
   for (i in 1:(n.modules-1)){
     module <- labels2colors(i)
-    # Plot module eigengene expression
-    par(mar = c(2, 1, 1, 1))
-    png(paste0(figfolder, '/ME_distribution_', i, '_', module, '.png'), width=1920, height=1080)
-    boxplot(ME.SNV[,paste0('ME', i)], ME.WT[,paste0('ME', i)], names=c('SNV', 'WT'),
-            col=module, xlab='group', ylab='ME value', cex.lab = 2, cex.axis = 2, cex.main = 2, cex.colorLabels = 2)
-    dev.off()
-    # Calculate if distribution of log2FC values between the groups are significantly different from zero in each module
-    #pval.modules <- data.frame()
     logvals <- d.log2fc[names(coex.net$colors[coex.net$colors==i]),]
     t.stat <- t.test(logvals)
     pvals.modules[module] <- t.stat$p.value
     print(c(module, median(logvals), sd(logvals), length(logvals)))
+    #    par(mar = c(2, 1, 1, 1))
+    #    png(paste0(figfolder, '/ME_distribution_', i, '_', module, '.png'), width=1920, height=1080)
+    #      boxplot(ME.SNV[,paste0('ME', i)], ME.WT[,paste0('ME', i)], names=c('SNV', 'WT'),
+    #              col=module, xlab='group', ylab='ME value', cex.lab = 2, cex.axis = 2, cex.main = 2, cex.colorLabels = 2)
+    #    dev.off()
   }
   # Multiple testing correction
   p.adj <- p.adjust(unlist(pvals.modules))
@@ -114,163 +78,4 @@ coexpression.analysis <- function(d, d.log2fc, outfolder, figfolder, i, stage, p
   write.table(pval.modules.adj, file=paste0(outfolder, '/sign_log2fc_modules.tsv'), sep='\t',
               row.names=TRUE, col.names=TRUE, quote=FALSE)
   return(list(coex.net, pval.modules.adj, power))
-}
-
-#' GO term enrichment analysis of the modules identified by WGCNA
-#' 
-#' @param coex.net Coexpression network generated by WGCNA
-#' @param IDs Protein identifier
-#' @param log2values Log2FC values for each of the proteins
-#' @param names Gene/protein names
-#' @param entrez.ids Entrez identifiers for the proteins
-#' @param fig.folder Directory to save figure files
-#' @param out.folder Output folder for data files
-#' @return Object with GO enrichment statistics per module and a heatmap showing the log2FC values of the top 10 enriched GO terms
-
-GO.terms.modules <- function(coex.net, IDs, log2values, entrez.ids, fig.folder, out.folder){
-  # GO enrichment analysis of the modules
-  #expr2ID <- match(ids, d.summary$Entry)
-  allEntrezIDs <- entrez.ids
-  names(allEntrezIDs) <- IDs
-  moduleColors <- labels2colors(coex.net$colors)
-  modules <- levels(as.factor(moduleColors))
-  n.modules <- length(modules)
-  for (i in 0:(n.modules-1)) {
-    # Save Entrez IDs per module
-    module <- labels2colors(i)
-    entrez.module <- entrez.ids[moduleColors==module]
-    filename <- paste0(out.folder, '/EntrezIDs_', i, '_', module, '.txt')
-    write.table(as.data.frame(entrez.module), file=filename, row.names=FALSE, col.names=FALSE)
-  }
-  # As background in the enrichment analysis, we will use all probes in the analysis.
-  filename <- paste0(out.folder, '/EntrezIDs_all.txt')
-  write.table(as.data.frame(allEntrezIDs), file=filename, row.names=FALSE, col.names=FALSE)
-  
-  # GO enrichment
-  # Save 100 most significant GO terms
-  GO.enrichment <- GOenrichmentAnalysis(moduleColors, entrez.ids, organism='human', nBestP=100, includeOffspring=TRUE)
-  enrichment.tab <- GO.enrichment$bestPTerms[[4]]$enrichment
-  write.table(enrichment.tab, file=paste0(out.folder, '/GOEnrichmentTable100.tsv'),
-              sep='\t', quote=FALSE, row.names=FALSE)
-  # Summary with most relevant columns only
-  rel.cols <- c(1, 2, 5, 6, 7, 11, 12, 13)
-  sub.enr.tab <- enrichment.tab[,rel.cols]
-  write.table(sub.enr.tab, file=paste0(out.folder, '/GOEnrichmentTable100_summary.tsv'),
-              sep='\t', quote=FALSE, row.names=FALSE)
-  
-  ### Plot heatmaps of the top ten enriched GO terms per module
-  signGOTerms <- data.frame(modules=GO.enrichment$bestPTerms$`BP, CC, MF`$enrichment$module, GOTerms=GO.enrichment$bestPTerms$`BP, CC, MF`$enrichment$termID)
-  
-  # Color palette for the heatmap
-  colors = c(seq(-5,-1,length=1000),seq(-.999999,.999999,length=1000),seq(1, 5,length=1000))
-  my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 2999)
-  
-  for (i in 1:(n.modules-1)){
-    module <- labels2colors(i)
-    #print(module)
-    # Select proteins in the module
-    inModule <- moduleColors == module
-    # Original IDs
-    idsModule <- IDs[inModule]
-    for (j in 1:length(idsModule)){
-      # If there is no gene name associated with the Uniprot ID, keep the uniprot ID
-      if (is.na(idsModule[j])){
-        idsModule[j] <- idsModule[j]
-      }
-    }
-    # Log2FC values of proteins in module
-    log2ModuleProteins <- log2values[idsModule]
-    #names(log2ModuleProteins) <- idsModule
-    # GO IDs significantly enriched in module
-    GOTermsModule <- signGOTerms[signGOTerms$modules == module, 'GOTerms']
-    # Entrez IDs of proteins in module
-    entrezModule <- entrez.ids[idsModule]
-    entrezModule.noNA <- unique(entrezModule[!is.na(entrezModule)])
-    # GO mapping
-    egGO <- org.Hs.egGO
-    # Include the offspring for each GO term
-    GO.offspr <- list(bp=as.list(GOBPOFFSPRING), cc=as.list(GOCCOFFSPRING), mf=as.list(GOMFOFFSPRING))
-    # All Entrez IDs that have at least one GO term
-    mappedProteins <- mappedkeys(egGO)
-    # Get the indeces of the Entrez IDs in our list
-    indeces <- match(entrezModule.noNA, mappedProteins)
-    if (sum(is.na(indeces)) != 0) {
-      entrezModule.noNA <- entrezModule.noNA[!is.na(indeces)]
-      indeces <- indeces[!is.na(indeces)]
-    }
-    annotations <- as.list(egGO[mappedProteins[indeces]])
-    
-    # For each GO term, count the number of Entrez IDs with that GO term and create a matrix
-    # to be plotted as a heatmap
-    d.GO <- data.frame(matrix(data=NA, ncol=sum(inModule), nrow=length(GOTermsModule)))
-    rownames(d.GO) <- unlist(lapply(GOTermsModule, function(x){GOTERM[[x]]@Term}))
-    colnames(d.GO) <- idsModule
-    GOcounts <- list()
-    
-    for (term in GOTermsModule) {
-      GOcounts[[term]] <- 0
-      # Check whether the current term or one of the direct children of the term is associated with the Entrez ID
-      children <- c(GO.offspr$bp[[term]], GO.offspr$cc[[term]], GO.offspr$mf[[term]])
-      for (entr in names(annotations)) {
-        if (is.na(entr)){
-          print('Entrez ID unknown')
-        }
-        if (term %in% names(annotations[[entr]]) | sum(children %in% names(annotations[[entr]])) > 0){
-          GOcounts[[term]] <- GOcounts[[term]] + 1
-          # Match column number
-          colnum <- match(entr, entrezModule)
-          d.GO[GOTERM[[term]]@Term, colnum] <- log2ModuleProteins[colnum]
-        }
-      }
-    }
-    # Sort columns by log2FC values
-    d.GO <- d.GO[,order(log2ModuleProteins)]
-    
-    # Check for very large overlap in GO terms (i.e. GO terms that are probably related)
-    #print(head(rownames(d.GO)))
-    terms.mapping <- AnnotationDbi::select(GO.db, keys=rownames(d.GO), columns='GOID', keytype='TERM')
-    terms.mapping$print <- rep(TRUE, nrow(terms.mapping))
-    for (j in 1:nrow(terms.mapping)) {
-      ID <- terms.mapping$GOID[j]
-      term <- terms.mapping$TERM[j]
-      children <- c(GO.offspr$bp[[ID]], GO.offspr$cc[[ID]], GO.offspr$mf[[ID]])
-      # Don't put a term in the heatmap if one of its direct descendants is also in the list
-      if (sum(children %in% terms.mapping$GOID > 0)){
-        terms.mapping$print[j] <- FALSE
-      }
-    }
-    #print(sum(terms.mapping$print))
-    #print(terms.mapping[terms.mapping$print,])
-    n.include <- sum(terms.mapping$print)
-    #print(n.include)
-    if (n.include > 10){
-      d.GO <- d.GO[terms.mapping$print,][1:10,]
-    } else {
-      d.GO <- d.GO[terms.mapping$print,]
-    }
-    #print(dim(d.GO))
-    
-    # Remove empty columns
-    d.GO <- d.GO[,apply(d.GO, 2, function(x){sum(!is.na(x)) > 0})]
-    
-    # Create heatmap of clusters
-    plotfile <- paste0(fig.folder, '/GO_heatmap_', i, '_', module, '.pdf')
-    ylab.size <- max(unlist(lapply(rownames(d.GO), nchar)))/15
-    h <- pheatmap(d.GO,
-                  cluster_rows=FALSE,
-                  cluster_cols=FALSE,
-                  legend=TRUE,
-                  color=my_palette,
-                  breaks=colors,
-                  show_rownames=TRUE,
-                  show_colnames=TRUE,
-                  filename=plotfile,
-                  width=ncol(d.GO)/5+ylab.size,
-                  height=nrow(d.GO)/4,
-                  na_col='#E3E3E3'
-    )
-    write.table(d.GO, file=paste0(out.folder, '/GO_heatmapvalues_', i, '_', module, '.tsv'),
-                sep='\t', quote=FALSE, row.names=TRUE, col.names=TRUE)
-  }
-  return(list(GO.enrichment, d.GO))
 }
--- src/similarity.R
+++ src/similarity.R
@@ -1,10 +1,17 @@
-jaccard_similarity <- function(A, B) {
-  intersection = length(intersect(A, B))
-  union = length(A) + length(B) - intersection
-  return (intersection/union)
-}
+#' Similarity analysis
+#' 
+#' @param folder1 Folder of exploration dataset output
+#' @param folder2 Folder of validation dataset output
+#' @param outfolder Output folder for similarity output
 
 similarity.analysis <- function(folder1, folder2, outfolder){
+  
+  jaccard_similarity <- function(A, B) {
+    intersection = length(intersect(A, B))
+    union = length(A) + length(B) - intersection
+    return (intersection/union)
+  }
+  
   #GSEA
   dir1 <- paste0(folder1, '/GO/')
   dir2 <- paste0(folder2, '/GO/')
@@ -15,8 +22,8 @@ similarity.analysis <- function(folder1, folder2, outfolder){
       SetA <- as.vector(t(read.table(paste0(dir1, 'GSEA_BP.tsv'), sep='\t', header = FALSE)))
       SetB <- as.vector(t(read.table(paste0(dir2, 'GSEA_BP.tsv'), sep='\t', header = FALSE)))
       Jaccard_Similarity <- jaccard_similarity(SetA,SetB)
-      gsea['BP',1] <- Jaccard_Similarity
-      print(paste('Similarity for GSEA BP', ':', Jaccard_Similarity))
+      gsea['BP',1] <- jaccard_similarity(SetA,SetB)
+      print(paste('Similarity for GSEA BP:', Jaccard_Similarity))
     }
   }
   
@@ -26,11 +33,83 @@ similarity.analysis <- function(folder1, folder2, outfolder){
       SetB <- as.vector(t(read.table(paste0(dir2, 'GSEA_MF.tsv'), sep='\t', header = FALSE)))
       Jaccard_Similarity <- jaccard_similarity(SetA,SetB)
       gsea['MF', 1] <- Jaccard_Similarity
-      print(paste('Similarity for GSEA MF', ':', Jaccard_Similarity))
+      print(paste('Similarity for GSEA MF:', Jaccard_Similarity))
     }
   }
   
   write.table(gsea, file=paste0(outfolder, '/gsea.tsv'), row.names=TRUE, col.names=TRUE, sep='\t')
+  
+  #DESEQ2
+  dir1 <- paste0(folder1, '/differential expression/')
+  dir2 <- paste0(folder2, '/differential expression/')
+  
+  DE <- matrix(0, ncol = 1, nrow = 1)
+  
+  DF1 <- read.table(paste0(dir1, 'DE_LFC.tsv'), sep='\t', header = TRUE, row.names = 1)
+  DF2 <- read.table(paste0(dir2, 'DE_LFC.tsv'), sep='\t', header = TRUE, row.names = 1)
+  
+  R1 <- rownames(DF1)
+  R2 <- rownames(DF2)
+  
+  DF2 <- DF2[rownames(DF2) %in% R1,]
+  DF1 <- DF1[rownames(DF1) %in% R2,]
+  
+  DF3 <- DF1-DF2
+  LFC <- abs(DF3)
+  print(paste('Similarity for DE genes', ':',  length(LFC[LFC <= 0.25])/length(DF3)))
+  
+  DE[1,1] <- length(LFC[LFC <= 0.25])/length(DF3)
+  
+  write.table(DE, file=paste0(outfolder, '/de.tsv'), row.names=TRUE, col.names=TRUE, sep='\t')
+  
+  DE2 <- matrix(0, ncol = 2, nrow = 5)
+  colnames(DE2) <- c('Positive', 'Negative')
+  rownames(DE2) <- c('Top 10', 'Top 50', 'Top 100', 'Top 500', 'Top 1000')
+  
+  geneList1 <- as.vector(t(read.table(paste0(dir1, 'DE_genes.tsv'), sep='\t', header=FALSE)))
+  geneList2 <- as.vector(t(read.table(paste0(dir1, 'DE_genes.tsv'), sep='\t', header=FALSE)))
+  
+  SetA <- geneList1[1:10]
+  SetB <- geneList2[1:10]
+  DE2['Top 10','Positive'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- geneList1[1:50]
+  SetB <- geneList2[1:50]
+  DE2['Top 50','Positive'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- geneList1[1:100]
+  SetB <- geneList2[1:100]
+  DE2['Top 100','Positive'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- geneList1[1:500]
+  SetB <- geneList2[1:500]
+  DE2['Top 500','Positive'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- geneList1[1:1000]
+  SetB <- geneList2[1:1000]
+  DE2['Top 1000','Positive'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- tail(geneList1,10)
+  SetB < tail(geneList2,10)
+  DE2['Top 10','Negative'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- tail(geneList1,50)
+  SetB <- tail(geneList2,50)
+  DE2['Top 50','Negative'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- tail(geneList1,100)
+  SetB <- tail(geneList2,100)
+  DE2['Top 100','Negative'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- tail(geneList1,500)
+  SetB <- tail(geneList2,500)
+  DE2['Top 500','Negative'] <- jaccard_similarity(SetA,SetB)
+  
+  SetA <- tail(geneList1,1000)
+  SetB <- tail(geneList2,1000)
+  DE2['Top 1000','Negative'] <- jaccard_similarity(SetA,SetB)
+  
+  write.table(DE2, file=paste0(outfolder, '/de2.tsv'), row.names=TRUE, col.names=TRUE, sep='\t')
   
   #MODULES
   dir1 <- paste0(folder1, '/coexpression/')
@@ -57,16 +136,15 @@ similarity.analysis <- function(folder1, folder2, outfolder){
         bestColor <- colorB
       }
     }
-    print(paste('Similarity for', colorA, 'and', bestColor, 'modules', ':', best))
+    print(paste('Similarity for', colorA, 'and', bestColor, 'modules:', best))
   }
   
   write.table(modules, file=paste0(outfolder, '/moduleSimilarity.tsv'), row.names=TRUE, col.names=TRUE, sep='\t')
   
-  #SIGNFICANT SUBNETWORKS
+  #SUBNETWORKS
   dir1 <- paste0(folder1, '/hotnet/HotNet_results/consensus_nodes_log2_')
   dir2 <- paste0(folder2, '/hotnet/HotNet_results/consensus_nodes_log2_')
   
-  #SUBNETWORKS
   colorsA <- as.vector(t(read.table(paste0(folder1, '/GO/hotnetSubnetworks.tsv'), sep='\t', header = FALSE)))
   colorsB <- as.vector(t(read.table(paste0(folder2, '/GO/hotnetSubnetworks.tsv'), sep='\t', header = FALSE)))
   exclusiveA <- colorsA[!(colorsA == 'grey')]
@@ -81,9 +159,8 @@ similarity.analysis <- function(folder1, folder2, outfolder){
       if (file.info(paste0(dir1, colorA, '.tsv'))$size == 0 || file.info(paste0(dir2, colorB, '.tsv'))$size == 0){
         next
       }
-      SetA <- as.vector(t(read.csv(paste0(dir1, colorA, '.tsv'), sep='\t', header = FALSE)[1,]))
-      
-      SetB <- as.vector(t(read.csv(paste0(dir2, colorB, '.tsv'), sep='\t', header = FALSE)[1,]))
+      SetA <- unlist(read.csv(paste0(dir1, colorA, '.tsv'), sep='\t', header = FALSE))
+      SetB <- unlist(read.csv(paste0(dir2, colorB, '.tsv'), sep='\t', header = FALSE))
       
       Jaccard_Similarity <- jaccard_similarity(SetA,SetB)
       subnetworks[colorB, colorA] <- Jaccard_Similarity
@@ -92,7 +169,7 @@ similarity.analysis <- function(folder1, folder2, outfolder){
         bestColor <- colorB
       }
     }
-    print(paste('Similarity for', colorA, 'and', bestColor, 'subnetworks', ':', best))
+    print(paste('Similarity for', colorA, 'and', bestColor, 'subnetworks:', best))
   }
   
   write.table(subnetworks, file=paste0(outfolder, '/subnetworkSimilarity.tsv'), row.names=TRUE, col.names=TRUE, sep='\t')
@@ -109,6 +186,7 @@ similarity.analysis <- function(folder1, folder2, outfolder){
   write.table(overallSimilarity, file=paste0(outfolder, '/similarity.tsv'), row.names=TRUE, sep='\t')
   
   if (file.info(paste0(folder1, '/GO/subnetworks.tsv'))$size != 0 && file.info(paste0(folder2, '/GO/subnetworks.tsv'))$size != 0) {
+    #SIGNFICANT SUBNETWORKS
     colorsA <- as.vector(t(read.table(paste0(folder1, '/GO/subnetworks.tsv'), sep='\t', header = FALSE)))
     colorsB <- as.vector(t(read.table(paste0(folder2, '/GO/subnetworks.tsv'), sep='\t', header = FALSE)))
     exclusiveA <- colorsA[!(colorsA == 'grey')]
@@ -123,9 +201,8 @@ similarity.analysis <- function(folder1, folder2, outfolder){
         if (file.info(paste0(dir1, colorA, '.tsv'))$size == 0 || file.info(paste0(dir2, colorB, '.tsv'))$size == 0){
           next
         }
-        SetA <- as.vector(t(read.csv(paste0(dir1, colorA, '.tsv'), sep='\t', header = FALSE)[1,]))
-        
-        SetB <- as.vector(t(read.csv(paste0(dir2, colorB, '.tsv'), sep='\t', header = FALSE)[1,]))
+        SetA <- unlist(read.csv(paste0(dir1, colorA, '.tsv'), sep='\t', header = FALSE))
+        SetB <- unlist(read.csv(paste0(dir2, colorB, '.tsv'), sep='\t', header = FALSE))
         
         Jaccard_Similarity <- jaccard_similarity(SetA,SetB)
         sSubnetworks[colorB, colorA] <- Jaccard_Similarity
@@ -134,12 +211,28 @@ similarity.analysis <- function(folder1, folder2, outfolder){
           bestColor <- colorB
         }
       }
-      print(paste('Similarity for', colorA, 'and', bestColor, 'signficant subnetworks', ':', best))
+      print(paste('Similarity for', colorA, 'and', bestColor, 'signficant subnetworks:', best))
+    }
+    
+    dir1 <- paste0(folder1, '/GO/')
+    dir2 <- paste0(folder2, '/GO/') 
+    for (colorA in exclusiveA){
+      for (colorB in exclusiveB){
+        for (n in nrow(read.csv(paste0(folder1, '/hotnet/HotNet_results/consensus_nodes_log2_', colorA, '.tsv'), sep='\t', header = FALSE))){
+          for (m in nrow(read.csv(paste0(folder2, '/hotnet/HotNet_results/consensus_nodes_log2_', colorB, '.tsv'), sep='\t', header = FALSE))){
+            try({SetA <- as.vector(t(read.table(paste0(dir1, colorA, 'BP', n, '.tsv'), sep='\t', header = FALSE)))}, silent=TRUE)
+            try({SetB <- as.vector(t(read.csv(paste0(dir2, colorB, 'BP', n, '.tsv'), sep='\t', header = FALSE)))}, silent=TRUE)
+            print(paste('Similarity for', colorA, 'and', colorB, 'BP:', jaccard_similarity(SetA,SetB)))
+            
+            try({SetA <- as.vector(t(read.table(paste0(dir1, colorA, 'MF', n, '.tsv'), sep='\t', header = FALSE)))}, silent=TRUE)
+            try({SetB <- as.vector(t(read.csv(paste0(dir2, colorB, 'MF', n, '.tsv'), sep='\t', header = FALSE)))}, silent=TRUE)
+            print(paste('Similarity for', colorA, 'and', colorB, 'MF:', jaccard_similarity(SetA,SetB)))
+          }
+        }
+      }
     }
     
     #GENES
-    dir1 <- paste0(folder1, '/GO/')
-    dir2 <- paste0(folder2, '/GO/') 
     SetA <- as.vector(t(read.csv(paste0(dir1, '/genes.tsv'), sep='\t', header = FALSE)))
     SetB <- as.vector(t(read.table(paste0(dir2, '/genes.tsv'), sep='\t', header = FALSE)))
     Jaccard_Similarity <- jaccard_similarity(SetA,SetB)
